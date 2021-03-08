78d6b936173019baca1a5dec5e9c2aa1a5fb9702
Asonglefac Millenium Nzefe <asonglefacmillenium@gmail.com>
2021-03-08 03:14:51 +0100

    Fetching from api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { useQuery } from "react-query";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//components
+import Drawer from "@material-ui/core/Drawer";
+import LinearProgress from "@material-ui/core/LinearProgress";
+import Grid from "@material-ui/core/Grid";
+import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
+import Badge from "@material-ui/core/Badge";
+
+//styles
+import { Wrapper } from "./styles";
+
+export type CartItemType {
+    id: number;
+    category: String;
+    description: String;
+    price: Number;
+    image: string;
+    amount: String;
+    title: string;
+}
+
+const getProducts = async(): Promise < CartItemType > =>
+    await (await fetch('https://fakestoreapi.com/products')).json();
+const App = () => {
+    return ( <
+        div className = "App" >
+        <
+        h1 > Start < /h1> < /
+        div >
+    );
 }
 
 export default App;
